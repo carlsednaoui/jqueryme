4ee8a4552233b7f50e6ec426eb20812feb17bcb1
carl sednaoui <carlsednaoui@gmail.com>
2013-07-16 11:52:10 -0400

    Add jqueryme.js

---
+++ jqueryme.js
@@ -0,0 +1,17 @@
+;(function() {
+  var v = '1.10.2';
+
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done      = false,
+    script        = document.createElement('script');
+    script.src    = 'http://ajax.googleapis.com/ajax/libs/jquery/' + v + '/jquery.min.js';
+
+    script.onload = script.onreadystatechange = function() {
+      if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+        done = true;
+      }
+    };
+    console.log("you've been jqueried");
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+})();
